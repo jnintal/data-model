7b46b3a3d989a523dc66d991b8fcf267cca0b500
Justin Intal <justin.intal@gmail.com>
2017-06-14 23:57:07 -0700

    commit v2

--- server.js
+++ server.js
@@ -10,11 +10,10 @@ const {Blog} = require('./models');
 const app = express();
 app.use(bodyParser.json());
 
-// GET requests to /restaurants => return 10 restaurants
+// GET requests to /posts
 app.get('/posts', (req, res) => {
   Blog
     .find()
-    .limit(20)
     // `exec` returns a promise
     .exec()
     // success callback: for each restaurant we got back, we'll
@@ -46,3 +45,115 @@ app.get('/posts/:id', (req, res) => {
         res.status(500).json({message: 'Internal server error'})
     });
 });
+
+// POST
+app.post('/posts', (req, res) => {
+
+  const requiredFields = ['author', 'content', 'title'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  Blog
+    .create({
+      author: req.body.author,
+      content: req.body.content,
+      title: req.body.title
+    })
+    .then(
+      blog => res.status(201).json(blog.apiRepr()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
+});
+
+// PUT
+app.put('/posts/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    res.status(400).json({message: 'Internal server error'});
+  }
+
+  const toUpdate = {};
+  const updateableFields = ['author', 'content', 'title'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Blog
+    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+    .exec()
+    .then(blog => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
+
+// DELETE
+
+app.delete('/posts/:id', (req, res) => {
+  Blog
+    .findByIdAndRemove(req.params.id)
+    .exec()
+    .then(blog => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
+app.use('*', function(req, res) {
+  res.status(404).json({message: 'Not Found'});
+});
+
+let server;
+
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+     return new Promise((resolve, reject) => {
+       console.log('Closing server');
+       server.close(err => {
+           if (err) {
+               return reject(err);
+           }
+           resolve();
+       });
+     });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
